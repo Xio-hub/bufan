90dccac51e496bbd5cf297a76300d80178d0abd6
Eric Xiong <653816325@qq.com>
2020-02-16 20:27:28 +0800

    api v1 update

--- app/Http/Controllers/Api/v1/SearchController.php
+++ app/Http/Controllers/Api/v1/SearchController.php
@@ -12,39 +12,51 @@ class SearchController extends Controller
 {
     public function search(Request $request,Product $product, Space $space, Style $style)
     {
-        $keyword = $request->input('keyword', '') ?? '';
-
-        if($keyword == ''){
-            return [];
-        }
-
-        $product_result = $product->select('id', 'name', 'cover')
-                                ->where(['name', 'like' , "%{$keyword}%"])
-                                ->get()
-                                ->toArray();
-
-        $space_result = $space->select('id', 'name', 'cover')
-                                ->where(['name', 'like' , "%{$keyword}%"])
-                                ->get()
-                                ->toArray();
-
-        $style_result = $style->select('id', 'name', 'cover')
-                                ->where(['name', 'like' , "%{$keyword}%"])
-                                ->get()
-                                ->toArray();
-                                
         $data = [
             'product' => [
                 'title' => '新品鉴赏',
-                'data' => $product_result,
+                'data' =>[
+                    [
+                        'id' => 1,
+                        'name' => '新品1',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                    [
+                        'id' => 2,
+                        'name' => '新品2',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                ]
             ],
             'space' => [
                 'title' => '功能空间',
-                'data' => $space_result
+                'data' =>[
+                    [
+                        'id' => 1,
+                        'name' => '空间1',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                    [
+                        'id' => 2,
+                        'name' => '空间2',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                ]
             ],
             'style' => [
                 'title' => '风格鉴赏',
-                'data' => $style_result
+                'data' =>[
+                    [
+                        'id' => 1,
+                        'name' => '风格1',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                    [
+                        'id' => 2,
+                        'name' => '风格2',  
+                        'cover' => 'http://imageurl.com'
+                    ],
+                ]
             ]
         ];
 
